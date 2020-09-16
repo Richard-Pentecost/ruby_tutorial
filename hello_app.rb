4fc14da91dbdd15dc8338ca1e2ed0467416d9adb
Richard Pentecost <richard.pentecost@madetech.com>
2020-09-16 07:58:02 +0100

    update variable name error

--- hello_app.rb
+++ hello_app.rb
@@ -3,6 +3,6 @@ require 'sinatra'
 get '/' do
   DAYNAMES = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
               'Thursday', 'Friday', 'Saturday']
-  daynames = DAYNAMES[Time.now.wday]
+  dayname = DAYNAMES[Time.now.wday]
   "Hello, world! Happy #{dayname}."
 end
