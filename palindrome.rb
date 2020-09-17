810459664f608523ae29d648067ccb50887dd9a1
Richard Pentecost <richard.pentecost@madetech.com>
2020-09-17 10:22:13 +0100

    Phrase class setup in palindrome.rb

--- palindrome.rb
+++ palindrome.rb
@@ -1,4 +1,20 @@
-def palindrome?(string)
-  process_content = string.downcase
-  process_content == process_content.reverse
+# def palindrome?(string)
+#   process_content = string.downcase
+#   process_content == process_content.reverse
+# end
+
+class Phrase 
+  attr_accessor :content
+  def initialize(text)
+    @content = text
+  end
+
+  def palindrome?
+    process_content = self.content.downcase
+    process_content == process_content.reverse
+  end
+
+  def louder
+    self.content.upcase
+  end
 end
